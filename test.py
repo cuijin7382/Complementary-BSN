c49d9689d275b6a1d8aa496e45a02c7da58ea573
cuijin7382 <120766164+cuijin7382@users.noreply.github.com>
2024-05-16 18:18:01 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,46 @@
+import argparse, os
+import time
+import torch
+
+from src.util.config_parse import ConfigParser
+from src.trainer import get_trainer_class
+
+
+
+def main():
+    # parsing configuration
+    args = argparse.ArgumentParser()
+    # args.add_argument('-s', '--session_name', default='MYAPBSN_DND',  type=str)
+    # args.add_argument('-c', '--config',       default='APBSN_DND',  type=str)
+    args.add_argument('-s', '--session_name', default='APBSN_SIDD', type=str)
+    args.add_argument('-c', '--config',       default='APBSN_SIDD',  type=str)
+    args.add_argument('-e', '--ckpt_epoch',   default=0,     type=int)
+    args.add_argument('-g', '--gpu',          default='0',  type=str)
+    args.add_argument(      '--pretrained',   default='Complementary-BSN_SIDD.pth',  type=str)
+    args.add_argument(      '--thread',       default=4,     type=int)
+    args.add_argument(      '--self_en',      action='store_true')
+    args.add_argument(      '--test_img',     default=None, type=str)
+    # args.add_argument(      '--test_img',     default='./621.png',  type=str)
+    args.add_argument(      '--test_dir',     default=None,  type=str)
+
+    args = args.parse_args()
+
+    assert args.config is not None, 'config file path is needed'
+    if args.session_name is None:
+        args.session_name = args.config # set session name to config file name
+
+    cfg = ConfigParser(args)
+
+    # device setting
+    if cfg['gpu'] is not None:
+        os.environ['CUDA_VISIBLE_DEVICES'] = cfg['gpu']
+
+    # intialize trainer
+    trainer = get_trainer_class(cfg['trainer'])(cfg)
+
+    # test
+    trainer.test()
+
+
+if __name__ == '__main__':
+    main()
